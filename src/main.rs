f766bfe21f8037adb685c0685a758584fcd07b22
ot07 <ot07neoki@gmail.com>
2022-08-16 19:35:08 +0900

    🚧 fix window size as don't know how to get it dynamically

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use app::App;
 fn main() {
     dioxus::desktop::launch_cfg(App, |c| {
         c.with_window(|w| {
-            w.with_resizable(true).with_inner_size(
+            w.with_resizable(false).with_inner_size(
                 dioxus::desktop::wry::application::dpi::LogicalSize::new(480.0, 120.0),
             )
         })
