08e8a30013e2fad6c46c07991881e770ad505bf1
ot07 <ot07neoki@gmail.com>
2022-08-16 19:38:11 +0900

    ✨ seek by click on progress bar

--- src/app.rs
+++ src/app.rs
@@ -10,7 +10,7 @@ extern crate anyhow;
 extern crate cpal;
 use crate::player::{self, PlayerTrait};
 
-fn format_time(time: u64) -> String {
+fn format_time(time: &u64) -> String {
     let min = time / 60;
     let sec = time % 60;
     format!("{}:{:02}", min, sec)
@@ -59,21 +59,27 @@ pub fn App(cx: Scope) -> Element {
             .add_and_play("./examples/square_120bpm_4bars_16bit_44100hz.wav");
     });
 
-    let elapsed_format = use_state(&cx, || "0:00".to_string());
-    let duration_format = use_state(&cx, || "0:00".to_string());
+    let elapsed = use_state(&cx, || 0);
+    let elapsed_format = format_time(elapsed.get());
+    let duration = use_state(&cx, || 0);
+    let duration_format = format_time(duration.get());
     let progress = use_state(&cx, || 0.);
+    let is_dragging_progress_bar = use_state(&cx, || false);
     
-    use_future(&cx, (interval, player, elapsed_format, duration_format, progress), |(interval, player, elapsed_format, duration_format, progress)| async move {
+    use_future(&cx, (interval, player, elapsed, duration, progress, is_dragging_progress_bar), |(interval, player, elapsed, duration, progress, is_dragging_progress_bar)| async move {
         loop {
             interval.write_silent().tick().await;
-            let elapsed = player.read().elapsed().as_secs();
-            let duration = player.read().duration().unwrap() as u64;
-            elapsed_format.set(format_time(elapsed));
-            duration_format.set(format_time(duration));
-            progress.set(elapsed as f64 / duration as f64 * 100.);
+
+            if !*is_dragging_progress_bar.get() {
+                let e = player.read().elapsed().as_secs();
+                let d = player.read().duration().unwrap() as u64;
+                elapsed.set(e);
+                duration.set(d);
+                progress.set(e as f64 / d as f64 * 100.);
+            }
         }
     });
-    
+
     cx.render(rsx!(
         link { href: "/src/style.css", rel: "stylesheet" }
         div { class: "h-screen bg-zinc-800 flex flex-col justify-center items-center",
@@ -90,18 +96,58 @@ pub fn App(cx: Scope) -> Element {
                 }
 
                 div {
-                    class: "flex-1 mx-3 py-2 group relative w-auto",
+                    class: "flex-1 mx-3 my-2 group relative w-auto",
 
                     div {
-                        class: "rounded-full w-full h-1 bg-zinc-500 absolute top-0 bottom-0 left-0 right-0 m-auto"
+                        class: "py-2",
+                        onmousedown: move |evt| {
+                            let x = evt.client_x - (24 + 32 + 12);
+                            let progress_bar_width = 480 - (24 + 32 + 12) * 2;
+
+                            let d = *duration.get() as f64;
+                            let e = x as f64 / progress_bar_width as f64 * d;
+
+                            elapsed.set(e as u64);
+                            progress.set(e / d * 100.);
+                            is_dragging_progress_bar.set(true);
+                        },
+                        onmousemove: move |evt| {
+                            if *is_dragging_progress_bar.get() {
+                                let x = evt.client_x - (24 + 32 + 12);
+                                let progress_bar_width = 480 - (24 + 32 + 12) * 2;
+
+                                let d = *duration.get() as f64;
+                                let e = x as f64 / progress_bar_width as f64 * d;
+
+                                elapsed.set(e as u64);
+                                progress.set(e / d * 100.);
+                            }
+                        },
+                        onmouseup: move |evt| {
+                            let x = evt.client_x - (24 + 32 + 12);
+                            let progress_bar_width = 480 - (24 + 32 + 12) * 2;
+
+                            let d = *duration.get() as f64;
+                            let e = x as f64 / progress_bar_width as f64 * d;
+
+                            elapsed.set(e as u64);
+                            progress.set(e / d * 100.);
+
+                            player.write_silent().seek(e as i64);
+
+                            is_dragging_progress_bar.set(false);
+                        },
+                        div {
+                            class: "rounded-full w-full h-1 bg-zinc-500 absolute top-0 bottom-0 left-0 right-0 m-auto"
+                        }
                     }
                     div {
                         width: "{progress}%",
-                        class: "rounded-full h-1 bg-zinc-300 group-hover:bg-sky-500 absolute top-0 bottom-0 left-0 right-0 my-auto"
+                        class: "rounded-full h-1 bg-zinc-300 group-hover:bg-sky-500 absolute top-0 bottom-0 left-0 right-0 my-auto pointer-events-none"
                     }
                     div {
                         left: "{progress}%",
-                        class: "rounded-full w-4 h-4 bg-zinc-300 opacity-0 group-hover:opacity-100 absolute top-0 bottom-0 my-auto -translate-x-[50%]"
+                        class: "rounded-full w-4 h-4 bg-zinc-300 opacity-0 group-hover:opacity-100 absolute top-0 bottom-0 my-auto -translate-x-[50%] pointer-events-none"
                     }
                 }
 
